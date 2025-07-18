a8b826fca95342f5ca1e5e68d052fbfdc9fc284c
jkeys089 <jon@t12y.com>
2025-07-18 10:15:35 -0700

    ensure we do not block on dependency output (#6)

--- composer/composer_test.go
+++ composer/composer_test.go
@@ -100,7 +100,7 @@ func TestRunAll_DependencyExistsFirst(t *testing.T) {
 	cfg := composer.Config{
 		Version: composer.Version,
 		Services: map[string]composer.ServiceConfig{
-			"b1": {Command: "sleep 0.3"},
+			"b1": {Command: "sleep 0.3 && echo 'ok'", ReadyOn: "ok"},
 			"s1": {Command: "sleep 0.2"},
 			"s2": {Command: "sleep 0.1", DependsOn: []string{"b1"}},
 		},
@@ -111,7 +111,7 @@ func TestRunAll_DependencyExistsFirst(t *testing.T) {
 		t.Errorf("error: %v", err)
 	}
 
-	c.EnableDebug()
+	// c.EnableDebug()
 
 	start := time.Now()
 
@@ -156,5 +156,56 @@ func TestLargeOutput(t *testing.T) {
 
 	if !strings.Contains(output, expectedOutput) {
 		t.Errorf("expected large output value not found in actual execution output:\nwant: '%s'\ngot '%s'", expectedOutput, output)
+	}
+}
+
+func TestDaemonDependencies(t *testing.T) {
+	const expectedOutput = "hello world"
+	const unexpectedOutput = "goodbye cruel world"
+
+	cfg := composer.Config{
+		Version: composer.Version,
+		Services: map[string]composer.ServiceConfig{
+			"s1": {
+				// the main service will exit immediately - however, it should wait on all dependencies to become ready
+				// before executing, and then any remaining dependency services can be killed
+				Command:   "echo 'hello from s1'",
+				DependsOn: []string{"d1"},
+			},
+			"d1": {
+				// the first child dependency will exit after 2 seconds but is ready immediately - this simulates a
+				// dependency that runs forever in the background and should be killed after the main service exits
+				Command:   "sleep 2 && echo '" + unexpectedOutput + "'",
+				DependsOn: []string{"d2"},
+			},
+			"d2": {
+				// the first grandchild dependency will signal ready after 1 second and then exit immediately - this
+				// ensures the main service actually blocks on all dependencies to become ready before executing
+				ReadyOn: expectedOutput,
+				Command: "sleep 1 && echo '" + expectedOutput + "'",
+			},
+		},
+	}
+
+	c, err := composer.New(cfg, "s1")
+	if err != nil {
+		t.Errorf("error: %v", err)
+	}
+
+	// c.EnableDebug()
+
+	output := captureStdoutStderr(func() { err = c.Run() })
+	if err != nil {
+		if !strings.Contains(err.Error(), "interrupted by user") {
+			t.Errorf("error running composer: %v", err)
+		}
+	}
+
+	if !strings.Contains(output, expectedOutput) {
+		t.Errorf("expected output value not found in actual execution output:\nwant: '%s'\ngot '%s'", expectedOutput, output)
+	}
+
+	if strings.Contains(output, unexpectedOutput) {
+		t.Errorf("unexpected output value found in actual execution output:\nunexpected: '%s'\ngot '%s'", unexpectedOutput, output)
 	}
 }
